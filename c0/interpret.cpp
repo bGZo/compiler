fe33e9577bd0fc840a61a15c44ae10b3e5e16923
bGZoCg <bgzocg@outlook.com>
2021-11-03 17:03:19 +0800

    :hammer: modify comments

--- c0/interpret.cpp
+++ c0/interpret.cpp
@@ -27,9 +27,9 @@ fct string_to_fct(string s){
 }
 
 struct instruction {
-    enum fct f;     // 操作码
-    int l;          // 层数
-    int a;          // 值
+    enum fct f;                                 /* 操作码*/
+    int l;                                      /* 层数*/
+    int a;                                      /* 值*/
 }code[codemaxsize];
 
 int base(int, int*, int);
@@ -49,12 +49,12 @@ int main(){
             cout << line << endl;
             istringstream is(line);
             is >> str1 >> str2 >> str3;
-            //cout << "("<<i<<") "<<str2 << " " << str3 << " " << str4 << endl;
+            //cout << "("<<i<<") "<<str2 << " " << str3 << " " << str4 << endl; /*TEST*/
             code[instructionsum].f = string_to_fct(str1);
             code[instructionsum].l = stoi(str2);
             code[instructionsum].a = stoi(str3);
             instructionsum++;
-            //out << line << endl; // TEST
+            //out << line << endl; /* TEST*/
         }
     }else {
         cout << "no such file" << endl;
@@ -67,10 +67,10 @@ int main(){
 
 
 void interpret(){
-    int p;                                  // 指令寄存器
-    int b;                                  // 基址寄存器
-    int t;                                  // 栈顶寄存器
-    struct instruction i;                   // 虚拟机代码段
+    int p;                                  /* 指令寄存器*/
+    int b;                                  /* 基址寄存器*/
+    int t;                                  /* 栈顶寄存器*/
+    struct instruction i;                   /* 虚拟机代码段*/
 
     cout << "----------start----------\n";
     t = 0; b = 0; p = 0;
@@ -84,7 +84,7 @@ void interpret(){
                 t++;
                 break;
             case LOD:
-                if(i.a==0&&i.l==0){         // LOD 0 0
+                if(i.a==0&&i.l==0){
                     s[t]=s[0];
                     t++;
                 }else {
@@ -94,18 +94,18 @@ void interpret(){
                 break;
             case STO:
                 t--;
-                if(i.a==0&&i.l==0){          // STO 0 0
+                if(i.a==0&&i.l==0){
                     s[0]=s[t];
                 }else {
                     s[base(i.l, s, b) + i.a] = s[t];
                 }
                 break;
-            case CAL:                       //存放当前信息, 更新指针地址
-                s[t] = base(i.l, s, b);     // 基址入栈
-                s[t + 1] = 0;               // 初始化本过程的基址
-                s[t + 2] = p;               // 当前指令指针入栈
-                b = t;                      // 改变基地址指针为新过程的基地址
-                p = i.a;                    // 跳转
+            case CAL:                       /* 存放当前信息, 更新指针地址*/
+                s[t] = base(i.l, s, b);     /* 基址入栈*/
+                s[t + 1] = 0;               /* 初始化本过程的基址*/
+                s[t + 2] = p;               /* 当前指令指针入栈*/
+                b = t;                      /* 改变基地址指针为新过程的基地址*/
+                p = i.a;                    /* 跳转*/
                 break;
             case INT:
                 t += i.a;
@@ -146,19 +146,19 @@ void interpret(){
                 t = b;
                 p = s[t + 2];
                 b = s[t];
-                if (p == 0) return;         // if b==0: return 0
+                if (p == 0) return;         /* NOTE: if b==0: return 0*/
                 break;
         }
-//        for(int i=0; i<stacksize; i++) cout<<s[i]<<' '; cout<<endl; // [TEST]
+    //    for(int i=0; i<stacksize; i++) cout<<s[i]<<' '; cout<<endl; /*TEST Stack*/
     } while (p != 0);
 }
 
-int base(int l, int* s, int b){
+int base(int l, int* s, int b){ /* 求得上层过程的基址 */
     int bt;
     bt = b;
     while (l > 0){
         bt = s[bt];
         l--;
     }
-    return bt; // 求得上层过程的基址
+    return bt;
 }
